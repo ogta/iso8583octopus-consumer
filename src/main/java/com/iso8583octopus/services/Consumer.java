bef1f52a0b9c66159bfbd3e33ebdcdad0f825512
ogta <ogtadev@gmail.com>
2020-07-19 20:55:00 +0300

    consumer application created

---
+++ src/main/java/com/iso8583octopus/services/Consumer.java
@@ -0,0 +1,22 @@
+package com.iso8583octopus.services;
+
+import java.io.IOException;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+	private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+	private static final String TOPIC = "67";
+
+    @KafkaListener(topics = TOPIC, groupId = "67")
+    public void consume(String message) throws IOException {
+        logger.info(String.format("Consumed message : %s", message));
+    }
+
+}
